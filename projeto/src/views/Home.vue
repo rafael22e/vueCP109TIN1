33fc370dc74e57a3f6c141fcb6c4648fadc243ad
Rafael Bevevino <rafaelrsbv@gmail.com>
2021-11-22 00:21:29 -0300

    Criando base

--- projeto/src/views/Home.vue
+++ projeto/src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SystemBar/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
+import SystemBar from '@/components/SystemBar.vue'
+
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    // HelloWorld,
+    SystemBar
   }
 }
 </script>
